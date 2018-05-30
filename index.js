8f7e6d8c21275f2c800a24ac2db20b2674e544eb
Saikia, Amlan <amlan.saikia@cerner.com>
2018-05-30 19:17:41 +0530

    Done with layer merging

--- index.js
+++ index.js
@@ -14,26 +14,38 @@ const initialize = () => {
     }, 1000);
 }
 
-const drawStreaks = (previousState) => {
-    const currentState = previousState;
+const copyState = (state) => {
+    return state.map((row) => {
+        return row.map((column) => {
+            return Object.assign({}, column);
+        });
+    });
+}
+
+const drawStreakState = (previous) => {
+    if(!previous || !previous.length) {
+        return getEmptyState();
+    }
+
+    const newState = copyState(previous);
 
     // Move the row data occupation state down by one line.
     // Ignore the last row state
     for (let rowIndex = rowCount - 2; rowIndex >= 0; rowIndex--) {
         for(let columnIndex = 0; columnIndex < columnCount; columnIndex++) {
             // Move down the state
-            currentState[rowIndex + 1][columnIndex].blockType = currentState[rowIndex][columnIndex].blockType;
+            newState[rowIndex + 1][columnIndex].blockType = newState[rowIndex][columnIndex].blockType;
 
             if (rowIndex === 0) {
-                switch(currentState[rowIndex + 1][columnIndex].blockType) {
+                switch(newState[rowIndex + 1][columnIndex].blockType) {
                     case BlockType.BLOCK:
-                        currentState[rowIndex][columnIndex].blockType = BlockType.TAIL_START;
+                        newState[rowIndex][columnIndex].blockType = BlockType.TAIL_START;
                         break;
                     case BlockType.TAIL_START:
-                        currentState[rowIndex][columnIndex].blockType = BlockType.TAIL_END;
+                        newState[rowIndex][columnIndex].blockType = BlockType.TAIL_END;
                         break;
                     default:
-                        currentState[rowIndex][columnIndex].blockType = BlockType.NONE
+                        newState[rowIndex][columnIndex].blockType = BlockType.NONE
                         break;
                 }
             }
@@ -41,59 +53,61 @@ const drawStreaks = (previousState) => {
 
         if (Math.random(0, 1) < 0.2) {
             const streakColumn = Math.floor(Math.random(0, 1) * columnCount);
-            currentState[rowIndex][streakColumn].blockType = BlockType.BLOCK;
+            newState[rowIndex][streakColumn].blockType = BlockType.BLOCK;
         }
     }
 
-    return currentState;
+    return newState;
 }
 
-const drawShip = (previousState, shipFrame) => {
+const getShipSprites = () => {
     const centerBlock = Math.floor(columnCount/2);
+    const shipStart = State.getEmptyState();
+    const shipNormal = State.getEmptyState();
+    const shipForward = State.getEmptyState();
 
-    const currentState = State.getEmptyState();
+    shipStart[rowCount - 1].blockType = BlockType.BLOCK;
 
-    switch(shipFrame) {
-        case 0:
-            currentState[rowCount - 1][centerBlock].blockType = BlockType.BLOCK;
-            break;
-        case 2:
-            currentState[rowCount - 3][centerBlock].blockType = BlockType.BLOCK;
-            currentState[rowCount - 2][centerBlock - 1].blockType = BlockType.BLOCK;
-            currentState[rowCount - 2][centerBlock].blockType = BlockType.BLOCK;
-            currentState[rowCount - 2][centerBlock + 1].blockType = BlockType.BLOCK;
-            break;
-        default:
-            currentState[rowCount - 2][centerBlock].blockType = BlockType.BLOCK;
-            currentState[rowCount - 1][centerBlock - 1].blockType = BlockType.BLOCK;
-            currentState[rowCount - 1][centerBlock].blockType = BlockType.BLOCK;
-            currentState[rowCount - 1][centerBlock + 1].blockType = BlockType.BLOCK;
-            break;
-    }
+    shipForward[rowCount - 3][centerBlock].blockType = BlockType.BLOCK;
+    shipForward[rowCount - 2][centerBlock - 1].blockType = BlockType.BLOCK;
+    shipForward[rowCount - 2][centerBlock].blockType = BlockType.BLOCK;
+    shipForward[rowCount - 2][centerBlock + 1].blockType = BlockType.BLOCK;
 
-    return State.mergeState(previousState, currentState);
+    shipNormal[rowCount - 2][centerBlock].blockType = BlockType.BLOCK;
+    shipNormal[rowCount - 1][centerBlock - 1].blockType = BlockType.BLOCK;
+    shipNormal[rowCount - 1][centerBlock].blockType = BlockType.BLOCK;
+    shipNormal[rowCount - 1][centerBlock + 1].blockType = BlockType.BLOCK;
+
+    return [shipStart, shipNormal, shipForward];
 }
 
 const startUpSequence = () => {
-    let previousState = State.getEmptyState();
+    const shipSprites = getShipSprites();
+    let previousStreakState;
     let shipFrame = 0;
 
-    setInterval(() => {
+    const shipFrameHandle = setInterval(() => {
         shipFrame++;
+
+        if (shipFrame > 2) {
+            shipFrame = 1;
+            clearInterval(shipFrameHandle);
+        }
     }, 300);
 
-    previousState[0][3].blockType = BlockType.BLOCK;
     const intervalHandle = setInterval(() => {
         // Clean the canvas
         Renderer.clearCanvas(arenaCanvas);
-        // Get the next frame
-        const nextState = drawShip(drawStreaks(previousState), shipFrame);
-        // Render the next frame
-        Renderer.renderFrame(nextState);
 
-        // Store the last frame for calculations
-        previousState = nextState;
-    }, drawDelay);
+        // Store the last streak state for calculations
+        previousStreakState = drawStreakState(previousStreakState);
+
+        // Ship layer
+        const shipLayer = [...shipSprites[shipFrame]];
+
+        // Render the next frame
+        Renderer.renderFrame(State.mergeState(previousStreakState, shipLayer));
+    }, 10);
 };
 
 initialize();
--- state.js
+++ state.js
@@ -14,24 +14,39 @@ const getEmptyState = () => {
     return stateMatrix;
 };
 
+const getPositionMask = () => {
+    const positionMatrix = [];
+    for (let i = 0; i < rowCount; i++) {
+        const innerArray = [];
+        for (let j = 0; j < columnCount; j++) {
+            innerArray.push({
+                top: i * blockHeight + offset,
+                left: j * blockWidth  + offset
+            });
+        }
+        positionMatrix.push(innerArray);
+    }
+    return positionMatrix;
+}
+
 const getStateData = (stateMatrix, rowIndex = 0, columnIndex = 0) => {
     return stateMatrix[rowIndex][columnIndex];
 }
 
-const mergeState = (previous, current) => {
+const mergeState = (previous, next) => {
     return previous.map((row, rowIndex) => {
-        const currentStateRow = current[rowIndex];
-        row.forEach((column, columnIndex) => {
-            if (currentStateRow[columnIndex].blockType !== BlockType.NONE) {
-                column.blockType = currentStateRow[columnIndex].blockType;
-            }
+        const nextRow = next[rowIndex];
+        const mergedRow = row.map((column, columnIndex) => {
+            const nextColumn = nextRow[columnIndex];
+            return Object.assign({}, column, nextColumn.blockType === BlockType.NONE ? {} : { blockType: nextColumn.blockType })
         });
-        return row;
+        return mergedRow;
     })
 };
 
 const State = {
     getEmptyState,
+    getPositionMask,
     getStateData,
     mergeState
 }
